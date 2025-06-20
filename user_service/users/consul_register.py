1624308fcb8de80114c1616915ed17312b280a59
Minh Triet <triettran332003@gmail.com>
2025-06-20 15:42:50 +0700

    setup: register user_service with consul

---
+++ user_service/users/consul_register.py
@@ -0,0 +1,22 @@
+import consul # type: ignore
+import os
+
+def register_to_consul():
+    CONSUL_HOST = os.getenv('CONSUL_HOST', 'consul')
+    CONSUL_PORT = int(os.getenv('CONSUL_PORT', 8500))
+
+    SERVICE_NAME = 'user_service'
+    SERVICE_ID = 'user_service_1'
+    SERVICE_ADDRESS = os.getenv('SERVICE_HOST', 'user_service')  # chính là service name trong docker-compose
+    SERVICE_PORT = int(os.getenv('SERVICE_PORT', 5000))          # port nội bộ container
+
+    c = consul.Consul(host=CONSUL_HOST, port=CONSUL_PORT)
+
+    c.agent.service.register(
+        name=SERVICE_NAME,
+        service_id=SERVICE_ID,
+        address=SERVICE_ADDRESS,
+        port=SERVICE_PORT
+    )
+
+    print(f"[Consul] Registered {SERVICE_NAME} at {SERVICE_ADDRESS}:{SERVICE_PORT}")
