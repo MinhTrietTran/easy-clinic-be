3801ae5634c22b27035d9cba8b231ce24f59dd7a
Minh Triet <triettran332003@gmail.com>
2025-06-27 16:08:27 +0700

    refactor: split register_service from view

---
+++ user_service/users/v1/services.py
@@ -0,0 +1,24 @@
+from ..models.user import CustomUser
+from ..models.patient import Patient
+
+def register_user(data):
+    user = CustomUser.objects.create_user(
+            email = data['email'],
+            password=data['password'],
+            role=data.get('role','patient'), # Default: patient
+            phone=data.get('phone', None),
+        )
+    if user.role == 'patient':
+        gender=data.get('gender','').lower()
+        gender_value = True if gender == 'female' else False
+        Patient.objects.create(
+            user=user,
+            first_name = data.get('first_name',''),
+            last_name=data.get('last_name', ''),
+            gender=gender_value,
+            DOB=data.get('dob',''),
+            address=data.get('address',''),
+            allergies=data.get('allergies',''),
+            chronic_diseases=data.get('choronic_diseases'),
+        )
+    return user
--- user_service/users/v1/views.py
+++ user_service/users/v1/views.py
@@ -2,33 +2,15 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from ..models.user import CustomUser
-from ..models.patient import Patient
+from .services import register_user
+
 
 # Create your views here.
 class RegisterView(APIView):
     def post(self, request):
         data = request.data
         try:
-            user = CustomUser.objects.create_user(
-                email = data['email'],
-                password=data['password'],
-                role=data.get('role','patient'), # Default: patient
-                phone=data.get('phone', None),
-            )
-            if user.role == 'patient':
-                gender=data.get('gender','').lower()
-                gender_value = True if gender == 'female' else False
-                Patient.objects.create(
-                    user=user,
-                    first_name = data.get('first_name',''),
-                    last_name=data.get('last_name', ''),
-                    gender=gender_value,
-                    DOB=data.get('dob',''),
-                    address=data.get('address',''),
-                    allergies=data.get('allergies',''),
-                    chronic_diseases=data.get('choronic_diseases'),
-                )
+            result = register_user(request.data)
             return Response({"message":"User registered successfully"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
