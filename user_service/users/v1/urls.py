ffafc5a0a581d7422561fb324a9d04c887791086
Minh Triet <triettran332003@gmail.com>
2025-06-27 18:23:55 +0700

    add: refresh and verify token

--- user_service/users/v1/urls.py
+++ user_service/users/v1/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import ( # type: ignore
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+
+    # JWT built-in
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
